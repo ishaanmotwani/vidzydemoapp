a5507a3b2d37214772dcf80cd0bcc7596433ff02
Ishaan Motwani <imotwani@deloitte.com>
2016-08-25 15:57:20 +0530

    first commit

---
+++ public/javascripts/vidzy.js
@@ -0,0 +1,75 @@
+var app = angular.module('Vidzy', ['ngResource', 'ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/add-video', {
+            templateUrl: 'partials/video-form.html',
+            controller: 'AddVideoCtrl'
+        })
+        .when('/video/:id', {
+	        templateUrl: 'partials/video-form.html',	
+	        controller: 'EditVideoCtrl'
+	    })
+	    .when('/video/delete/:id', {
+	        templateUrl: 'partials/video-delete.html',	
+	        controller: 'DeleteVideoCtrl'
+	    })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+    function($scope, $resource){
+    	var Videos = $resource('/api/videos');
+        Videos.query(function(videos){
+            $scope.videos = videos;
+        });
+	}
+]);
+
+app.controller('EditVideoCtrl', ['$scope', '$resource','$routeParams', '$location',
+    function($scope, $resource, $routeParams, $location){
+    	var Videos = $resource('/api/videos/:id', { id: '@_id' }, {
+            update: { method: 'PUT' }
+        });
+        Videos.get({id:$routeParams.id},function(video){
+            $scope.video = video;
+        });
+
+        $scope.save = function(){
+            Videos.update($scope.video, function(){
+                $location.path('/');
+            });
+        };
+	}
+]);
+
+app.controller('DeleteVideoCtrl', ['$scope', '$resource','$routeParams', '$location',
+    function($scope, $resource, $routeParams, $location){
+    	var Videos = $resource('/api/videos/:id');
+        Videos.get({id:$routeParams.id},function(video){
+            $scope.video = video;
+        });
+
+        $scope.delete = function(){
+            Videos.delete({id:$routeParams.id}, function(video){
+                $location.path('/');
+            });
+        };
+	}
+]);
+
+app.controller('AddVideoCtrl', ['$scope', '$resource', '$location',
+    function($scope, $resource, $location){
+    	$scope.save = function(){
+            var Videos = $resource('/api/videos');
+            Videos.save($scope.video, function(){
+                $location.path('/');
+            });
+        };
+	}
+]);
